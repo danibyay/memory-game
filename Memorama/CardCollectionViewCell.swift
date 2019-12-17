8380c0e768de72114375a26f44d48014b90d02b7
Daniela Becerra <madajiji@gmail.com>
2019-12-17 12:15:03 -0600

    feat: Add class for the card cell

---
+++ Memorama/CardCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CardCollectionViewCell.swift
+//  Memorama
+//
+//  Created by Daniela Becerra Gonzalez on 17/12/19.
+//  Copyright © 2019 Daniela Becerra Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var frontImageView: UIImageView!
+    @IBOutlet weak var backImageView: UIImageView!
+}
