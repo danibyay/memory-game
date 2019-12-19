1de6b6299e7130dc51347907bab8d05ad58c2bb1
Daniela Becerra <madajiji@gmail.com>
2019-12-19 15:38:32 -0600

    feat: Add game timer and finished alert message

--- Memorama/ViewController.swift
+++ Memorama/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     var model = CardModel()
     var cardArray = [Card]()
     var firstFlippedCardIndex:IndexPath?
+    var timer:Timer?
+    var milliseconds:Float = 10000
     
+    @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
@@ -21,7 +24,8 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         cardArray = model.getCards()
         collectionView.delegate = self
         collectionView.dataSource = self
-        // Do any additional setup after loading the view, typically from a nib.
+        timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(timerElapsed), userInfo: nil, repeats: true)
+        RunLoop.main.add(timer!, forMode: .commonModes)
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,6 +33,25 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - Timer methods
+    @objc func timerElapsed() {
+        milliseconds -= 1
+        let seconds = String(format: "%.2f", milliseconds/1000)
+        if milliseconds >= 0 {
+            timerLabel.text = "Time remaining: \(seconds)"
+        }
+        
+        if milliseconds < 0 {
+            // stop the timer
+            timer?.invalidate()
+            timerLabel.textColor = UIColor.red
+            
+            // check if the player lost
+            checkGameEnded()
+            
+        }
+    }
+    
     // MARK: - UICollectionView Protocol Methods
     func collectionView(_ collectionView: UICollectionView,
             numberOfItemsInSection section: Int) -> Int {
@@ -47,6 +70,11 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(_ collectionView: UICollectionView,
             didSelectItemAt indexPath: IndexPath) {
+        
+        // check if it's valid to play
+        if milliseconds <= 0 {
+            return
+        }
         
         let cell = collectionView.cellForItem(at: indexPath) as! CardCollectionViewCell
         let card = cardArray[indexPath.row]
@@ -86,6 +114,9 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             firstCell?.remove()
             secondCell?.remove()
             
+            // every time there is a pair, it could be the last one, check!
+            checkGameEnded()
+            
         } else {
             // set the status of the model
             firstCard.isFlipped = false
@@ -104,6 +135,48 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         // reset the index to keep playing
         firstFlippedCardIndex = nil
         
+    }
+    
+    func checkGameEnded() {
+        // Determine if there are unmatched cards
+        var wonTheGame = true
+        for card in cardArray {
+            if !card.isMatched {
+                wonTheGame = false
+                break
+            }
+        }
+        
+        // Messaging variables
+        var title = ""
+        var message = ""
+        
+        // win condition
+        if wonTheGame {
+            if milliseconds > 0 {
+                timer?.invalidate()
+            }
+            title = "Congratulations!"
+            message = "You've won"
+        } else {
+            // lose condition
+            if milliseconds > 0 {
+                return
+            }
+            title = "Game Over"
+            message = "You've lost"
+        }
+        
+        showAlert(title, message)
+        
+    }
+    
+    func showAlert(_ title:String, _ message:String) {
+        // notify user
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(alertAction)
+        present(alert, animated: true, completion: nil)
     }
     
 } // End ViewControler Class
