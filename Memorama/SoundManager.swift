53fc161881101f01ec1f8f1c05a82efb34d9a950
Daniela Becerra <madajiji@gmail.com>
2020-01-10 12:49:08 -0600

    feat: Add sound files and new sound manager class

---
+++ Memorama/SoundManager.swift
@@ -0,0 +1,27 @@
+//
+//  SoundManager.swift
+//  Memorama
+//
+//  Created by Daniela Becerra Gonzalez on 10/01/20.
+//  Copyright © 2020 Daniela Becerra Gonzalez. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class SoundManager {
+    
+    var audioPlayer:AVAudioPlayer?
+    
+    enum SoundEffect {
+        
+        case flip
+        case shuffle
+        case match
+        case nomatch
+    }
+    
+    func playSound() {
+        
+    }
+}
